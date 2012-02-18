a9ee9fdefeba57c9ac54d970dbcd476dab99a70a
Tucker Bickler <tuckersworld@gmail.com>
2012-02-18 17:42:19 -0600

    changed event to songs:refresh

--- public/js/sockets.client.js
+++ public/js/sockets.client.js
@@ -1,9 +1,5 @@
 var socket = io.connect('http://localhost');
 
-socket.on('tweets:all', function(data) {
-	interactions.populateTracks(data.tracks);
-});
-
-socket.on('tweets:update', function(data) {
-	interactions.updateTracks(data.tracks);
+socket.on('songs:refresh', function(data) {
+	interactions.populateTracks(data);
 });
