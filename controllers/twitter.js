afe40e78e150b68557e5f05dbe7a9477e3760d98
Tucker Bickler <tuckersworld@gmail.com>
2012-02-18 16:21:31 -0600

    socket handling

--- controllers/twitter.js
+++ controllers/twitter.js
@@ -1,25 +1,25 @@
 module.exports = function(app) {
 
-  var Song = require('../models').Song,
-      Twitter = require('ntwitter');
+  var Song = require('../models').Song
+  //     Twitter = require('ntwitter');
 
-  var twitter = new Twitter({
-    consumer_key: process.env.TWITTER_KEY,
-    consumer_secret: process.env.TWITTER_SECRET,
-    access_token_key: process.env.TWITTER_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_TOKEN_SECRET
-  });
+  // var twitter = new Twitter({
+  //   consumer_key: process.env.TWITTER_KEY,
+  //   consumer_secret: process.env.TWITTER_SECRET,
+  //   access_token_key: process.env.TWITTER_TOKEN_KEY,
+  //   access_token_secret: process.env.TWITTER_TOKEN_SECRET
+  // });
 
-  twitter.stream('statuses/filter', { track: '#spotify' }, function(stream) {
-    stream.on('data', function (data) {
-      var body = data.text;
+  // twitter.stream('statuses/filter', { track: '#spotify' }, function(stream) {
+  //   stream.on('data', function (data) {
+  //     var body = data.text;
 
-      tweet = body ? body.match(/^#nowplaying ([^\–]+) – ([^\–]+) on #Spotify[\s]?(.+)$/i) : false;
+  //     tweet = body ? body.match(/^#nowplaying ([^\–]+) – ([^\–]+) on #Spotify[\s]?(.+)$/i) : false;
 
-      if(tweet && tweet.length > 2) {
-        console.log(tweet[1]);
-      }
-    });
-  });
+  //     if(tweet && tweet.length > 2) {
+  //       console.log(tweet[1]);
+  //     }
+  //   });
+  // });
 
 };
--- public/js/sockets.client.js
+++ public/js/sockets.client.js
@@ -1,7 +1,9 @@
 var socket = io.connect('http://localhost');
 
-socket.on('connect', function() {
-	socket.emit('tweet:all', function(data) {
-		console.log(data);
-	});
+socket.on('tweets:all', function(data) {
+	interactions.populateTracks(data.tracks);
+});
+
+socket.on('tweets:update', function(data) {
+	interactions.updateTracks(data.tracks);
 });
