5605bf43b66ae34ed06a1cf006c178556322309b
Tucker Bickler <tuckersworld@gmail.com>
2012-02-18 14:36:52 -0600

    added mongohq database

--- app.js
+++ app.js
@@ -24,7 +24,18 @@ app.configure('production', function(){
 });
 
 //Mongo connect
-app.db = mongoose.connect('mongodb://localhost/top-music');
+/**
+ * To get all the info to login, sign into your MongoHQ account, go to the db you want,
+ * click the "Database Info" tab, then look for the line that looks like:
+ * -------------------------------------------------------------
+ * mongodb://<user>:<password>@staff.mongohq.com:10056/node-test
+ * ---------|     |-|        |------------------|    |-|       |
+ *           USER    PASSWORD                    PORT   DB NAME
+ *
+ * ALSO, for testing, you should manually add a document and collection into MongoHQ
+ * from their "Add a Collection" > "Add a Document" links, then below we'll log it.
+ */
+app.db = mongoose.connect('mongodb://tuckbick:hackdayftw123@staff.mongohq.com:10097/twitter-top-40');
 
 //Controllers
 app.controllers = require('./controllers')(app);
