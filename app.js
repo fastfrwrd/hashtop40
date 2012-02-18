8a982822484adbacd424c8c736738b8eccc88921
Tucker Bickler <tuckersworld@gmail.com>
2012-02-18 12:35:10 -0600

    streaming sample code

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var express = require('express'),
     EventEmitter = require('events'),
-    mongoose = require('mongoose');
-
-var app = module.exports = express.createServer();
+    mongoose = require('mongoose'),
+    app = module.exports = express.createServer();
 
 // Configuration
 
@@ -23,13 +22,58 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+var config = {
+    user: "",
+    password: "",
+    track: ["#nodetwitter"]};
+
+var socket = require('socket.io').listen(app),
+    twitter = new (require("twitter-node").TwitterNode)(config);
+
+
+
+
+socket.addListener('clientMessage', function(data, client){
+  client.sockets.send(data);
+});
+
+twitter
+    .addListener('error', function(error){ // Always check for errors or they popup client side
+                     console.log(error.message);
+                 })
+    .addListener('tweet', function(tweet){ // A new tweet that matches the criteria has been located
+          socket.emit('clientMessage', tweet, socket);
+                 })
+    .addListener('limit', function(limit){ // New limit has been sent from the API
+                     sys.puts('LIMIT: ' + sys.inspect(limit));
+                 })
+    .addListener('delete', function(del){ // A delete event occured
+                     sys.puts('DELETE: ' + sys.inspect(del));
+                 })
+    .addListener('end', function(resp){ // API disconnect
+                     sys.puts('wave goodbye...' + resp.statusCode);
+                 })
+    .stream();
+
+
+
 
 //Mongo connect
 app.db = mongoose.connect('mongodb://localhost/top-music');
 
-// Routes
+
+
+
 app.routes = require('./controllers');
 
+// Routes
+app.get("/", function(req, res){
+
+  // Render the layout
+  res.render('index', {
+    title: "Amy and Steve Wedding"
+  });
+});
 
 //Init
 app.listen(3000);
